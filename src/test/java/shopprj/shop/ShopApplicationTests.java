a4a01f7b9315c63fb40f60012cb9c31de472834d
junyoung kim <adad568@naver.com>
2022-06-17 20:35:01 +0900

    Initial commit

---
+++ src/test/java/shopprj/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package shopprj.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
