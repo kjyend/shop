366ed705e421d2551c05ecf06d47b796753cf81d
junyoung kim <adad568@naver.com>
2022-08-28 22:27:42 +0900

    Setting7-commit

---
+++ src/main/java/shopprj/shop/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,31 @@
+package shopprj.shop.web.argumentresolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import shopprj.shop.domain.dto.MemberDto;
+import shopprj.shop.web.SessionConst;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasMemberType = MemberDto.class.isAssignableFrom(parameter.getParameterType()); //같은 타입인지 체크
+        return hasLoginAnnotation && hasMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request=(HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+        if(session==null){
+            return null;
+        }
+        return session.getAttribute(SessionConst.LOGIN_MEMBER);
+    }
+}
