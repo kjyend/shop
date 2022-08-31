27ecacc71eab0be68c3d27a1552d4c1c309cbc37
junyoung kim <adad568@naver.com>
2022-08-31 22:07:59 +0900

    item-commit

--- src/main/java/shopprj/shop/domain/dto/ItemDto.java
+++ src/main/java/shopprj/shop/domain/dto/ItemDto.java
@@ -1,4 +1,21 @@
 package shopprj.shop.domain.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import shopprj.shop.domain.entity.Item;
+
+@Builder
+@Getter
 public class ItemDto {
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    public Item toItemEntity(ItemDto itemDto) {
+        return Item.builder()
+                .name(itemDto.getName())
+                .price(itemDto.getPrice())
+                .stockQuantity(itemDto.getStockQuantity()).build();
+    }
 }
--- src/main/java/shopprj/shop/domain/entity/Item.java
+++ src/main/java/shopprj/shop/domain/entity/Item.java
@@ -1,6 +1,10 @@
 package shopprj.shop.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shopprj.shop.domain.dto.ItemDto;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,7 +12,21 @@ import javax.persistence.Id;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
     @Id @GeneratedValue
     private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    public ItemDto toItemDto(Item item) {
+        return ItemDto.builder()
+                .name(item.getName())
+                .price(item.getPrice())
+                .stockQuantity(item.getStockQuantity()).build();
+    }
 }
