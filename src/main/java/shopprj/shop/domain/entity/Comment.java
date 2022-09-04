d88deeb6164b7b1e503566580e4307f08560de1c
junyoung kim <adad568@naver.com>
2022-09-04 21:53:37 +0900

    Setting8-commit

---
+++ src/main/java/shopprj/shop/domain/entity/Comment.java
@@ -0,0 +1,16 @@
+package shopprj.shop.domain.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Comment {
+    @Id @GeneratedValue
+    private Long id;
+
+
+}
---
+++ src/main/java/shopprj/shop/domain/entity/ManageItem.java
@@ -0,0 +1,15 @@
+package shopprj.shop.domain.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class ManageItem {
+    @Id @GeneratedValue
+    private Long id;
+
+}
