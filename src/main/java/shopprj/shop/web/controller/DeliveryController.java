360d5201b6ef37dd9f687a9144dc5840e675e525
junyoung kim <adad568@naver.com>
2022-09-26 22:02:44 +0900

    html4-commit

--- src/main/java/shopprj/shop/web/controller/DeliveryController.java
+++
@@ -1,7 +0,0 @@
-package shopprj.shop.web.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class DeliveryController {
-}
--- src/main/java/shopprj/shop/web/controller/OrderController.java
+++ src/main/java/shopprj/shop/web/controller/OrderController.java
@@ -5,17 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import shopprj.shop.domain.dto.ItemDto;
+import shopprj.shop.domain.dto.MemberDto;
+import shopprj.shop.web.argumentresolver.Login;
 
 @Controller
 public class OrderController {
 
     @GetMapping("/Buy")
-    public String BuyForm(ItemDto itemDto, Model model){
-
+    public String BuyForm(@Login MemberDto loginMember, ItemDto itemDto, Model model){
+        model.addAttribute("item",itemDto);
         return "buy/Buy";
     }
     @PostMapping("/Buy")
-    public String Buy(){
+    public String Buy(){//총 가격, 주소 넣어서,
         return "redirect:/";
     }
 
