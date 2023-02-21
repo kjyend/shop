ea2349fec3eeb69e8a279f3cc249687de4cfcf22
junyoung kim <adad568@naver.com>
2023-02-21 22:08:00 +0900

    Update : 불필요한 구조 삭제

--- src/main/java/shopprj/shop/web/controller/ManagerController.java
+++ src/main/java/shopprj/shop/web/controller/ManagerController.java
@@ -7,7 +7,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import shopprj.shop.dto.ItemCreateDto;
 import shopprj.shop.dto.ItemDto;
